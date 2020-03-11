21fa0af225d91aa6da991c82feb6fedb8ffb6177
Torvalds Du <torvalds@ekohe.com>
2020-03-11 23:56:41 +0800

    7-8 7-9 至少看十遍啊 网络请求层封装(redux 中间件)

---
+++ src/redux/middleware/api.js
@@ -0,0 +1,82 @@
+import { get } from '../../utils';
+export const FETCH_DATA = 'FETCH DATA';
+export default store => next => action => {
+  const callAPI = action[FETCH_DATA];
+  if (typeof callAPI === 'undefined') {
+    return next(action);
+  }
+
+  const { endpoint, schema, types } = callAPI;
+
+  if (typeof endpoint !== 'string') {
+    throw new Error('endpoint 必须为字符串类型的 URL');
+  }
+
+  if (!schema) {
+    throw new Error('必须指定领域实体的 schema');
+  }
+
+  if (!Array.isArray(types) || types.length !== 3) {
+    throw new Error('需要指定一个包含了 3 个 action type 的数组');
+  }
+  if (types.any(type => typeof type === 'string')) {
+    throw new Error('action type 必须是为字符串类型');
+  }
+
+  const actionWith = data => {
+    const finalAction = { ...action, ...data };
+    delete finalAction[FETCH_DATA];
+    return finalAction;
+  };
+
+  const [requestType, successType, failureType] = types;
+
+  next(actionWith({ type: requestType }));
+
+  return fetchData(endpoint, schema).then(
+    response =>
+      next(
+        actionWith({
+          type: successType,
+          response
+        })
+      ),
+    error =>
+      next(
+        actionWith({
+          type: failureType,
+          error: error.message || '获取数据失败'
+        })
+      )
+  );
+};
+
+// 执行网络请求
+const fetchData = (endpoint, schema) => {
+  return get(endpoint).then(data => {
+    return normalizeData(data, schema);
+  });
+};
+
+// 根据 schema, 将获取的数据扁平化处理
+
+const normalizeData = (data, schema) => {
+  const { id, name } = schema;
+
+  let kvObj = {};
+  let ids = [];
+
+  if (Array.isArray(data)) {
+    data.forEach(item => {
+      kvObj[item[id]] = item;
+      ids.push(item[id]);
+    });
+  } else {
+    kvObj[data[id]] = data;
+    ids.push(data[id]);
+  }
+  return {
+    [name]: kvObj,
+    ids
+  };
+};
