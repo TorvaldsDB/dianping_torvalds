3861f4df9692080d5254ad0d7b8eb90538e197d7
Torvalds Du <torvalds@ekohe.com>
2020-03-11 22:44:00 +0800

    7-5 状态模块定义

---
+++ src/redux/modules/entities/index.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux';
+import comments from './comments';
+import orders from './orders';
+import products from './products';
+import shops from './shops';
+
+// 合并领域状态
+const rootReducer = combineReducers({
+  comments,
+  orders,
+  products,
+  shops
+});
