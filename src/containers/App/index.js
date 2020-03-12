ff6c70d4418f78c16a6346e5c7d65d238b677b59
Torvalds Du <torvalds@ekohe.com>
2020-03-12 10:00:12 +0800

    7-10 通用错误处理

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import ErrorToast from '../../components/ErrorToast';
 import './style.css';
+import { actions as appActions, getError } from '../../redux/modules/app';
 
-function App() {
-  return <div className='App'></div>;
+class App extends Component {
+  render() {
+    const {
+      error,
+      appActions: { clearError }
+    } = this.props;
+    return (
+      <div className='App'>
+        {error ? (
+          <ErrorToast msg={error} clearError={clearError}></ErrorToast>
+        ) : null}
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    error: getError(state)
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    appActions: bindActionCreators(appActions, dispatch)
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
