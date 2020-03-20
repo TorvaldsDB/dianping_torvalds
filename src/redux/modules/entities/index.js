1f6c3419f3ccb195aed6fd76af177976e3d76432
Torvalds Du <torvalds@ekohe.com>
2020-03-21 00:06:38 +0800

    10-6 设计搜索页 action

--- src/redux/modules/entities/index.js
+++ src/redux/modules/entities/index.js
@@ -3,13 +3,15 @@ import comments from './comments';
 import orders from './orders';
 import products from './products';
 import shops from './shops';
+import keywords from './keywords';
 
 // 合并领域状态
 const rootReducer = combineReducers({
   comments,
   orders,
   products,
-  shops
+  shops,
+  keywords
 });
 
 export default rootReducer;
--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -3,13 +3,15 @@ import entities from './entities';
 import home from './home';
 import app from './app';
 import detail from './detail';
+import search from './search';
 
 // 合并成根reducer
 const rootReducer = combineReducers({
   entities,
   home,
   detail,
-  app
+  app,
+  search
 });
 
 export default rootReducer;
