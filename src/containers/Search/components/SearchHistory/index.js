777222cd0d8f318edf707aceda1148af28b667de
Torvalds Du <torvalds@ekohe.com>
2020-03-20 09:35:10 +0800

    10-4 搜索历史组件

--- src/containers/Search/components/SearchHistory/index.js
+++ src/containers/Search/components/SearchHistory/index.js
@@ -1,7 +1,45 @@
 import React, { Component } from 'react';
+import './style.css';
 
-export default class SearchHistory extends Component {
-  render() {
-    return <div></div>;
+class SearchHistory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: ['烤鸭', '火锅', '面条']
+    };
   }
+
+  render() {
+    return (
+      <div className='searchHistory'>
+        <div className='searchHistory__header'>搜索记录</div>
+        <ul className='searchHistory__list'>
+          {this.state.data.map((item, index) => {
+            return (
+              <li
+                key={index}
+                onClick={this.handleClick}
+                className='searchHistory__item'
+              >
+                {item}
+              </li>
+            );
+          })}
+        </ul>
+        <div className='searchHistory__clear' onClick={this.handleClear}>
+          清除搜索记录
+        </div>
+      </div>
+    );
+  }
+
+  handleClick = () => {};
+
+  handleClear = () => {
+    this.setState({
+      data: []
+    });
+  };
 }
+
+export default SearchHistory;
