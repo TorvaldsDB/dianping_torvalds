eeaaab401716fa3a6224e5694a4cebce24c8715d
Torvalds Du <torvalds@ekohe.com>
2020-03-22 15:29:05 +0800

    12-4 连接 redux

--- src/containers/Login/components/LoginForm/index.js
+++ src/containers/Login/components/LoginForm/index.js
@@ -3,12 +3,18 @@ import './style.css';
 
 class LoginForm extends Component {
   render() {
+    const { username, password, onChange, onSubmit } = this.props;
     return (
       <div className='loginForm'>
         <div className='loginForm__inputContainer'>
           <div className='loginForm__row'>
             <label className='loginForm__mobileLabel'>86</label>
-            <input className='loginForm__input' name='username'></input>
+            <input
+              className='loginForm__input'
+              name='username'
+              value={username}
+              onChange={onChange}
+            ></input>
           </div>
           <div className='loginForm__row'>
             <label className='loginForm__passwordLabel'>密码</label>
@@ -16,11 +22,15 @@ class LoginForm extends Component {
               className='loginForm__input'
               name='password'
               type='password'
+              value={password}
+              onChange={onChange}
             ></input>
           </div>
         </div>
         <div className='loginForm__btnContainer'>
-          <button className='loginForm__btn'>登录</button>
+          <button className='loginForm__btn' onClick={onSubmit}>
+            登录
+          </button>
         </div>
       </div>
     );
