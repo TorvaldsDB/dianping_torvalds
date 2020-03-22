ff02aced936935b6f9889eac11b5142c6b8dcae6
Torvalds Du <torvalds@ekohe.com>
2020-03-22 21:30:56 +0800

    13-3 OrderItem 组件

--- src/containers/User/index.js
+++ src/containers/User/index.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import UserMain from './components/UserMain';
+import UserHeader from './components/UserHeader';
 
-export default class User extends Component {
+class User extends Component {
   render() {
     return (
       <div>
+        <UserHeader onBack={this.handleBack} onLogout={this.handleLogout} />
         <UserMain />
       </div>
     );
   }
+
+  handleBack = () => {
+    // todo
+  };
+
+  handleLogout = () => {
+    // todo
+  };
 }
+
+export default User;
