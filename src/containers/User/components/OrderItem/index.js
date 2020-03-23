2d3f86d1aad13a1d49484db8ec3b06fc0c149c6f
Torvalds Du <torvalds@ekohe.com>
2020-03-23 22:25:23 +0800

    13-11 订单评价 UI 实现

--- src/containers/User/components/OrderItem/index.js
+++ src/containers/User/components/OrderItem/index.js
@@ -30,9 +30,52 @@ class OrderItem extends Component {
             </div>
           </div>
         </div>
+        {this.renderEditArea()}
       </div>
     );
   }
+
+  //渲染订单评价区域的DOM
+  renderEditArea() {
+    return (
+      <div className='orderItem__commentContainer'>
+        <textarea
+          className='orderItem__comment'
+          onChange={this.handleCommentChange}
+          value={''}
+        />
+        {this.renderStars()}
+        <button className='orderItem__commentBtn' onClick={null}>
+          提交
+        </button>
+        <button className='orderItem__commentBtn' onClick={null}>
+          取消
+        </button>
+      </div>
+    );
+  }
+
+  renderStars() {
+    return (
+      <div>
+        {[1, 2, 3, 4, 5].map((item, index) => {
+          const lightClass = 3 >= item ? 'orderItem__star--light' : '';
+          return (
+            <span
+              className={'orderItem__star ' + lightClass}
+              key={index}
+              onClick={null}
+            >
+              ★
+            </span>
+          );
+        })}
+      </div>
+    );
+  }
+
+  //评价信息发生变化
+  handleCommentChange = () => {};
 
   //删除订单
   handleRemove = () => {
